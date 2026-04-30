5a53c1a1956cabb6b9e895644a78a25294880637
Ilya Kamens <ilyakamens@gmail.com>
2026-04-30 22:28:06 +0200

    Fix e2e tests

--- client/playwright.config.ts
+++ client/playwright.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
-  webServer: {
-    command: 'pnpm build && pnpm preview',
-    url: 'https://scaffold-preview.localhost',
-    reuseExistingServer: !process.env.CI,
-  },
+  webServer: process.env.CI
+    ? {
+        command: 'pnpm build && vite preview --port 4173',
+        url: 'http://localhost:4173',
+      }
+    : {
+        command: 'pnpm build && pnpm preview',
+        url: 'https://scaffold-preview.localhost',
+        reuseExistingServer: true,
+      },
   use: {
     ignoreHTTPSErrors: true,
   },
